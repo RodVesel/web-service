963d50b62a9c7da9e92ae3f6c5caf773da2a9e47
Rodion Veselovskii <veselovskiy4@gmail.com>
2023-11-08 01:25:01 +0300

    add servlet mirror add templates for page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,20 @@
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        MirrorServlet mirrorServlet = new MirrorServlet();
+
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.addServlet(new ServletHolder(mirrorServlet), "/mirror");
 
         Server server = new Server(8080);
-
+        server.setHandler(servletContextHandler);
+        server.start();
+        server.join();
     }
 }
---
+++ src/main/java/MirrorServlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MirrorServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(req);
+//        pageVariables.put("key", "");
+
+        resp.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("key", request.getParameterMap().toString());
+        return pageVariables;
+    }
+}
